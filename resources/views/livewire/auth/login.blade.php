c42efec0b490bdcf6083b041069f622dc80bfb6a
BrunoDeBrito <brunordebrito@gmail.com>
2024-12-27 17:50:29 -0300

    be able to successfully log in

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,3 +1,7 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
+    @error('invalidCredentials')
+        <span class="error">{{ $message }}</span>
+    @enderror
 </div>
--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -11,7 +11,7 @@ it('renders successfully', function () {
 
 it('should be able to login', function () {
     $user = User::factory()->create([
-        'email' => 'joe@doe',
+        'email'    => 'joe@doe.com',
         'password' => 'password',
     ]);
 
@@ -22,7 +22,33 @@ it('should be able to login', function () {
         ->assertHasNoErrors()
         ->assertRedirect(route('dashboard'));
 
-    expect(auth()->user())->toBeTrue()
-        ->and(auth()->user()->id)->toBe($user->id);
+    expect(auth()->check())->toBeTrue()
+        ->and(auth()->user())->id->toBe($user->id);
+});
 
+it('should make sure to inform the user an error when email and password doesnt work', function () {
+    Livewire::test(Login::class)
+        ->set('email', 'joe@doe.com')
+        ->set('password', 'password')
+        ->call('tryToLogin')
+        ->assertHasErrors(['invalidCredentials'])
+        ->assertSee(trans('auth.failed'));
+});
+
+it('should make sure that the rate limiting is blocking after 5 attempts', function () {
+    $user = User::factory()->create();
+
+    for ($i = 0; $i < 5; $i++) {
+        Livewire::test(Login::class)
+            ->set('email', $user->email)
+            ->set('password', 'wrong-password')
+            ->call('tryToLogin')
+            ->assertHasErrors(['invalidCredentials']);
+    }
+
+    Livewire::test(Login::class)
+        ->set('email', $user->email)
+        ->set('password', 'wrong-password')
+        ->call('tryToLogin')
+        ->assertHasErrors(['rateLimiter']);
 });

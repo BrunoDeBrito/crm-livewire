4d0f9a5b9cea6e8d0e6249fd02fbb6f5530b9bd7
BrunoDeBrito <brunordebrito@gmail.com>
2025-01-08 17:59:55 -0300

    responsividade ajustada no lado do cliente

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 //region Login Flow
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
-Route::get('/logout', fn() => auth()->logout());
+Route::get('/logout', fn () => auth()->logout());
 Route::get('/password/recovery', Password\Recovery::class)->name('password.recovery');
 Route::get('/password/reset', Password\Reset::class)->name('password.reset');
 //endregion
@@ -18,12 +18,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
 
     //region Admin
-    Route::prefix('/admin')
-        ->middleware('can:' . Can::BE_AN_ADMIN->value)
-        ->group(function () {
-            Route::get('/dashboard', Admin\Dashboard::class)
-                ->name('admin.dashboard');
-        });
+    Route::prefix('/admin')->middleware('can:' . Can::BE_AN_ADMIN->value)->group(function () {
+        Route::get('/dashboard', Admin\Dashboard::class)->name('admin.dashboard');
+
+        Route::get('/users', Admin\Users\Index::class)->name('admin.users');
+    });
     //endregion
 });
 //endregion
